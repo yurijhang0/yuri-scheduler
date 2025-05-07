c9c4c259fba6be3389af864bf5668492fef2a91c
yurijhang0 <yurijhang0@gmail.com>
2025-05-07 19:39:17 -0400

    Add changes

--- src/js/main.js
+++ src/js/main.js
@@ -8,27 +8,32 @@ const containerScheduler = document.querySelector('.scheduler-container');
 let containerSchedulerRows = null;
 // Buttons
 const btnScheduleLeft = document.querySelector('.scheduler-btn_left');
-const btnScheduleRight = document.querySelector('.scheduler-btn_left');
+const btnScheduleRight = document.querySelector('.scheduler-btn_right');
+
+// -----------------------Dummy Data-----------------------
+const month1 = [];
+for (let i = 0; i < 31; i++) {
+  month1.push({
+    dayNum: i + 1,
+  });
+}
+const month2 = [];
+for (let i = 0; i < 30; i++) {
+  month2.push({
+    dayNum: i + 1,
+  });
+}
+const month3 = [];
+for (let i = 0; i < 29; i++) {
+  month3.push({
+    dayNum: i + 1,
+  });
+}
+let currMonth = month2;
+let currScheduleIndex = 1;
+const scheduleArray = [month1, month2, month3];
 
 // -----------------------Behavior-----------------------
-// TEMP DATA
-// const user
-const days = [];
-for (let i = 0; i < 31; i++) {
-  days.push({
-    dayNum: i + 1,
-  });
-}
-const day2 = [];
-for (let i = 0; i < 30; i++) {
-  day2.push({
-    dayNum: i + 1,
-  });
-}
-let currDays = day2;
-let currDayIndex = 1;
-const scheduleArray = [days, day2];
-
 const displaySchedule = function (days) {
   // Clear schedule before populating
   containerScheduler.innerHTML = '';
@@ -62,15 +67,60 @@ const displaySchedule = function (days) {
     row.insertAdjacentHTML('beforeend', containerDaysHTML);
   });
 };
-displaySchedule(day2);
 
+const modifyBtn = function (btn, direction) {
+  const btnClassEnable = `scheduler-btn_${direction}_enable`;
+  const btnClassDisable = `scheduler-btn_${direction}_disable`;
+
+  // Button should be enabled or disabled based on if there are more schedules
+  const toDisable =
+    direction === 'left'
+      ? currScheduleIndex === 0
+      : currScheduleIndex === scheduleArray.length - 1;
+  const btnClassList = btn.classList;
+  if (toDisable) {
+    btnClassList.remove(btnClassEnable);
+    btnClassList.add(btnClassDisable);
+  } else {
+    btnClassList.remove(btnClassDisable);
+    btnClassList.add(btnClassEnable);
+  }
+};
+
+// Left arrow btn event listener
 btnScheduleLeft.addEventListener('click', function (e) {
   e.preventDefault();
 
-  console.log('here');
+  console.log('here1');
 
-  if (currDayIndex > 0) {
-    currDayIndex -= 1;
+  if (currScheduleIndex > 0) {
+    currScheduleIndex--;
   }
-  displaySchedule(scheduleArray[currDayIndex]);
+  modifyBtn(btnScheduleLeft, 'left');
+  modifyBtn(btnScheduleRight, 'right');
+  displaySchedule(scheduleArray[currScheduleIndex]);
 });
+
+// Right arrow btn event listener
+btnScheduleRight.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  console.log('here2');
+
+  // Increase current schedule index
+  if (currScheduleIndex < scheduleArray.length - 1) {
+    currScheduleIndex++;
+  }
+  modifyBtn(btnScheduleLeft, 'left');
+  modifyBtn(btnScheduleRight, 'right');
+  displaySchedule(scheduleArray[currScheduleIndex]);
+});
+
+const firstRender = function () {
+  console.log('First render');
+
+  modifyBtn(btnScheduleLeft, 'left');
+  modifyBtn(btnScheduleRight, 'right');
+  displaySchedule(month2);
+};
+firstRender();
